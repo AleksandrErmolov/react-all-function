b6a84452d3765d92d6028beb2455d37cf68c492d
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-02-06 11:11:45 +0300

    add first action

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './styles/App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='post'>
+        <div className='post__content'>
+        <strong>1. Javascript</strong>
+          <div>
+            Javascript - язык программирования
+          </div>
+        </div>
+        <div className='post__btns'>
+          <button>Удалить</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
